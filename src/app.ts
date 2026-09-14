1de8c4f097dbd823dc40e41d2a74d675f55f7b0e
Caio <cvdinizramos@gmail.com>
2026-09-14 18:43:02 -0300

    add : model CheckIn

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,10 @@
 import fastify from "fastify";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "./generated/prisma/client.js";
+import { env } from "./env/index.js";
 
 export const app = fastify();
+
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+
+export const prisma = new PrismaClient({ adapter });
