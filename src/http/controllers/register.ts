82a3fa1b5f26789e9be5355d9fa5dd3a19195c48
Caio <cvdinizramos@gmail.com>
2026-09-17 20:26:02 -0300

    create : userWithSameEmail

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -14,11 +14,21 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const password_hash = await hash(password, 6);
 
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userWithSameEmail) {
+    return reply.status(409).send();
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password_hash,
+      password_hash,
     },
   });
 
