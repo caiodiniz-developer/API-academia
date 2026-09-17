a5602650861781055b016af30ec9a05a6947913c
Caio <cvdinizramos@gmail.com>
2026-09-17 20:32:11 -0300

    fix: menssage userWithSameEmail

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -21,7 +21,7 @@ export async function registerUseCase({
   });
 
   if (userWithSameEmail) {
-    return reply.status(409).send();
+    throw new Error('Email already existe.')
   }
 
   await prisma.user.create({
