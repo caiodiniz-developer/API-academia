fda5a86b151238dda6c9c15487d02ebe3b1ffed3
Caio <cvdinizramos@gmail.com>
2026-09-17 20:23:22 -0300

    create: password_hash\

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,5 +1,6 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
 import z from "zod";
+import { hash } from "bcryptjs";
 import { prisma } from "../../lib/prisma.js";
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -11,11 +12,13 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
+  const password_hash = await hash(password, 6);
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash: password_hash,
     },
   });
 
