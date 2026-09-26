80ff1090ea5b468bbc02f42f64a2a204901510e8
Caio <cvdinizramos@gmail.com>
2026-09-26 11:16:04 -0300

    create if (env.NODE_ENV

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import { appRoutes } from "./http/routes.js";
 import { ZodError } from "zod";
+import { env } from "./env/index.js";
 
 export const app = fastify();
 
@@ -11,6 +12,9 @@ app.setErrorHandler((error, request, reply) => {
     return reply
       .status(400)
       .send({ message: "Validation error.", issues: error.format() });
+  }
+  if (env.NODE_ENV !== "production") {
+    console.error(error);
   }
 
   return reply.status(500).send({ message: "Internal server error." });
