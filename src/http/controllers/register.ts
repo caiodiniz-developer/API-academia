3a030192641ac61fecebd940653b8895914ec4e9
Caio <cvdinizramos@gmail.com>
2026-09-26 11:08:20 -0300

    add message in err

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -26,7 +26,7 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
     });
   } catch (err) {
     if (err instanceof UserAlreadyExistsError) {
-      return reply.status(409).send();
+      return reply.status(409).send({ message: err.message });
     }
 
     return reply.status(500).send(); //TODO: fix
